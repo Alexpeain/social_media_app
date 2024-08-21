608e785bf19499a55daec9cb405fb6888069bd1b
alexpeain <tonghsengsom@email.com>
2024-08-21 21:37:29 +0630

    first commit

---
+++ core/routers.py
@@ -0,0 +1,27 @@
+from rest_framework import routers
+from core.user.views import UserViewSet
+from core.auth.viewsets import (
+    RegisterViewSet,
+    LoginViewSet,
+    RefreshViewSet,
+    # LogoutViewSet,
+)
+router = routers.SimpleRouter()
+
+
+# ##################################################################### #
+# ################### AUTH                       ###################### #
+# ##################################################################### #
+
+router.register(r"auth/register", RegisterViewSet, basename="auth-register")
+router.register(r"auth/login", LoginViewSet, basename="auth-login")
+router.register(r"auth/refresh", RefreshViewSet, basename="auth-refresh")
+# router.register(r"auth/logout", LogoutViewSet, basename="auth-logout")
+
+# ##################################################################### #
+# ################### USER                       ###################### #
+# ##################################################################### #
+
+router.register(r"user", UserViewSet, basename="user")
+
+urlpatterns = [*router.urls, ]
